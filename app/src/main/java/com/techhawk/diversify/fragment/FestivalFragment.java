a8e909bd2eec24b4cd116113a8b2abfacae559cf
Wuyidi <eddiewuyidi@gmail.com>
2018-03-26 09:05:09 +1100

    read data from firebase

--- app/src/main/java/com/techhawk/diversify/fragment/FestivalFragment.java
+++ app/src/main/java/com/techhawk/diversify/fragment/FestivalFragment.java
@@ -3,18 +3,39 @@ package com.techhawk.diversify.fragment;
 
 import android.os.Bundle;
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.techhawk.diversify.R;
+import com.techhawk.diversify.model.Holiday;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FestivalFragment extends BaseFragment {
 
+    // Instance variables
+    private SearchView searchView;
+    private ListView festivalView;
+    private DatabaseReference festivalReference;
 
     public FestivalFragment() {
         // Required empty public constructor
@@ -24,8 +45,52 @@ public class FestivalFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_festival, container, false);
+        View currentView = inflater.inflate(R.layout.fragment_festival, container, false);
+        festivalView = currentView.findViewById(R.id.festival_list_view);
+        // Connect to Firebase database
+        festivalReference = FirebaseDatabase.getInstance().getReference().child("holidays");
+        // Set up FirebaseListAdapter
+        FirebaseListOptions<Holiday> options = new FirebaseListOptions.Builder<Holiday>()
+                .setQuery(festivalReference, Holiday.class).setLayout(R.layout.list_festival_item).setLifecycleOwner(this).build();
+        FirebaseListAdapter<Holiday> adapter = new FirebaseListAdapter<Holiday>(options) {
+            @Override
+            protected void populateView(View v, Holiday holiday, int position) {
+                ((TextView) v.findViewById(R.id.list_festival_name)).setText(holiday.getName());
+                ((TextView) v.findViewById(R.id.list_festival_comments)).setText(holiday.getComments());
+            }
+        };
+        festivalView.setAdapter(adapter);
+
+        festivalView.setEmptyView(currentView.findViewById(R.id.empty_festival));
+
+
+        return currentView;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.search_view, menu);
+        setSearchView(menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    // Set up search view
+    private void setSearchView(Menu menu) {
+        MenuItem item = menu.getItem(0);
+        searchView = new SearchView(getContext());
+        searchView.setIconifiedByDefault(true);
+        searchView.setQueryHint("Festival Name");
+        searchView.setSubmitButtonEnabled(false);
+        item.setActionView(searchView);
+
     }
 
 }
--- app/src/main/java/com/techhawk/diversify/model/Holiday.java
+++ app/src/main/java/com/techhawk/diversify/model/Holiday.java
@@ -1,17 +1,68 @@
 package com.techhawk.diversify.model;
 
 
+import com.google.firebase.database.IgnoreExtraProperties;
 /**
  * Created by Yidi Wu on 22/3/18.
  */
 
+@IgnoreExtraProperties
 public class Holiday {
     // Instance variable
-    private String holidayName;
-    private String country;
-    private String imageUrl;
-    private String HolidayType;
+    private String imgUrl;
+    private String type;
     private String comments;
+    private String history;
+    private String name;
 
+    public Holiday() {
+    }
 
+    public Holiday(String imgUrl, String type, String comments, String history, String name) {
+        this.imgUrl = imgUrl;
+        this.type = type;
+        this.comments = comments;
+        this.history = history;
+        this.name = name;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String getHistory() {
+        return history;
+    }
+
+    public void setHistory(String history) {
+        this.history = history;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
